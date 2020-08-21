11d1a680587d9af300436c75de18c891124921c6
Leo <m5pro@M1-MacPro.local>
2020-08-21 19:53:27 +0800

    leo

--- CSharpGenerater/BaseFrame.py
+++ CSharpGenerater/BaseFrame.py
@@ -17,7 +17,7 @@ import wx.xrc
 class BaseFrame ( wx.Frame ):
 
 	def __init__( self, parent ):
-		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = wx.EmptyString, pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"C#生成工具", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
 
 		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
 
@@ -25,13 +25,16 @@ class BaseFrame ( wx.Frame ):
 
 		bSizer2 = wx.BoxSizer( wx.HORIZONTAL )
 
-		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"输出位置", wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.m_staticText1 = wx.StaticText( self, wx.ID_ANY, u"项目位置", wx.DefaultPosition, wx.DefaultSize, 0 )
 		self.m_staticText1.Wrap( -1 )
 
 		bSizer2.Add( self.m_staticText1, 0, wx.ALL, 5 )
 
 		self.m_outputDirPicker = wx.DirPickerCtrl( self, wx.ID_ANY, wx.EmptyString, u"选择输出文件的路径", wx.DefaultPosition, wx.DefaultSize, wx.DIRP_DEFAULT_STYLE|wx.DIRP_SMALL )
 		bSizer2.Add( self.m_outputDirPicker, 1, wx.ALL, 5 )
+
+		self.m_button8 = wx.Button( self, wx.ID_ANY, u"配置", wx.DefaultPosition, wx.Size( 45,-1 ), 0 )
+		bSizer2.Add( self.m_button8, 0, wx.ALL, 5 )
 
 
 		bSizer1.Add( bSizer2, 0, wx.EXPAND, 5 )
@@ -95,10 +98,13 @@ class BaseFrame ( wx.Frame ):
 
 		bSizer1.Add( bSizer4, 0, wx.EXPAND, 5 )
 
-		bSizer3 = wx.BoxSizer( wx.VERTICAL )
+		bSizer3 = wx.BoxSizer( wx.HORIZONTAL )
 
-		self.m_button1 = wx.Button( self, wx.ID_ANY, u"开始", wx.DefaultPosition, wx.DefaultSize, 0 )
-		bSizer3.Add( self.m_button1, 1, wx.ALL|wx.EXPAND, 5 )
+		self.m_button1 = wx.Button( self, wx.ID_ANY, u"生产代码", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer3.Add( self.m_button1, 1, wx.ALL, 5 )
+
+		self.m_button4 = wx.Button( self, wx.ID_ANY, u"删除代码", wx.DefaultPosition, wx.DefaultSize, 0 )
+		bSizer3.Add( self.m_button4, 1, wx.ALL, 5 )
 
 
 		bSizer1.Add( bSizer3, 0, wx.EXPAND, 5 )
@@ -113,14 +119,44 @@ class BaseFrame ( wx.Frame ):
 		self.Centre( wx.BOTH )
 
 		# Connect Events
+		self.m_outputDirPicker.Bind( wx.EVT_DIRPICKER_CHANGED, self.OnProjectDirChanged )
+		self.m_button8.Bind( wx.EVT_BUTTON, self.OnClickOption )
 		self.m_button1.Bind( wx.EVT_BUTTON, self.OnClickStart )
+		self.m_button4.Bind( wx.EVT_BUTTON, self.OnClickDelete )
 
 	def __del__( self ):
 		pass
 
 
 	# Virtual event handlers, overide them in your derived class
+	def OnProjectDirChanged( self, event ):
+		event.Skip()
+
+	def OnClickOption( self, event ):
+		event.Skip()
+
 	def OnClickStart( self, event ):
 		event.Skip()
+
+	def OnClickDelete( self, event ):
+		event.Skip()
+
+
+###########################################################################
+## Class OptionFrame
+###########################################################################
+
+class OptionFrame ( wx.Frame ):
+
+	def __init__( self, parent ):
+		wx.Frame.__init__ ( self, parent, id = wx.ID_ANY, title = u"配置", pos = wx.DefaultPosition, size = wx.Size( 500,300 ), style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHints( wx.DefaultSize, wx.DefaultSize )
+
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
 
 
--- CSharpGenerater/GenCSFrame.py
+++ CSharpGenerater/GenCSFrame.py
@@ -5,6 +5,7 @@
 ###########################################################################
 
 import sys
+import shutil
 from BaseFrame import *
 from GenerateCSharp import *
 
@@ -23,11 +24,21 @@ class Redirect:
 		pass
 
 
+folderFlag = 'AutoGenCS_'
+
+
 class GenCSFrame(BaseFrame):
 
 	def __init__(self, parent):
 		BaseFrame.__init__(self, parent)
+		self.InitPath()
 		Redirect(self.m_logTextCtrl)
+
+	def InitPath(self):
+		self.project_path = '/Users/m5pro/Documents/M5-C/release-2.0'
+		self.m_outputDirPicker.SetPath(self.project_path)
+		self.script_path = os.path.join(self.project_path, 'Assets/Scripts')
+		self.plugin_path = os.path.join(self.project_path, 'Assets/Plugins')
 
 	def RefreshGenCount(self):
 		classMin = int(self.m_classMinTextCtrl.GetValue())
@@ -48,23 +59,44 @@ class GenCSFrame(BaseFrame):
 		pass
 
 	def OnClickStart(self, event):
-		self.RefreshGenCount()
-		path = self.m_outputDirPicker.GetPath()
-		if path == '':
-			self.ShowMessageDialog('错误', '请选择输出路径')
+		global folderFlag
+		if not (os.path.exists(self.script_path) and os.path.exists(self.plugin_path)):
+			self.ShowMessageDialog('错误', '工程目录不存在, 请重新选择')
 			return
+		self.DoGenCSFile(self.script_path)
+		self.DoGenCSFile(self.plugin_path)
+
+	def DoGenCSFile(self, output):
+		self.RefreshGenCount()
 		space = GetNameSpace()
-		outdir = os.path.join(path, space)
-		if not os.path.exists(outdir):
-			os.makedirs(outdir)
+		output_dir = os.path.join(output, folderFlag + '_' + space)
+		if not os.path.exists(output_dir):
+			os.makedirs(output_dir)
 		for i in range(0, self.classCount):
 			info = ClassGenerater(space, self.methodCount, self.attrCount, self.varCount)
-			path = os.path.join(outdir, '%s.cs' % info.name)
+			path = os.path.join(output_dir, '%s.cs' % info.name)
 			with open(path, 'w+') as f:
 				f.write(PrintClass(info))
 				print '生成文件: %s' % path
 		print '========生成完成,类:{0},函数:{1},属性:{2},变量:{3}========'.format(
 			self.classCount, self.methodCount, self.attrCount, self.varCount)
+
+	def OnClickDelete(self, event):
+		self.DeleteFolder(self.script_path)
+		self.DeleteFolder(self.plugin_path)
+
+	def DeleteFolder(self, path):
+		for folder in os.listdir(path):
+			if folder.startswith(folderFlag):
+				dirt = os.path.join(path, folder)
+				shutil.rmtree(dirt)
+				print '删除文件夹:%s' % dirt
+
+	def OnProjectDirChanged(self, event):
+		self.project_path = self.m_outputDirPicker.GetPath()
+		self.script_path = os.path.join(self.project_path, 'Assets/Scripts')
+		self.plugin_path = os.path.join(self.project_path, 'Assets/Plugins')
+		print ('工程目录:%s' % self.project_path)
 
 	def ShowMessageDialog(self, title, message):
 		dlg = wx.MessageDialog(None, message, title, wx.OK)

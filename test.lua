56e3fb21dc8baddb46cb016d8b332e4894c1905e
Leo <m5pro@M1-MacPro.local>
2020-09-23 09:58:39 +0800

    leo

---
+++ test.lua
@@ -0,0 +1,247 @@
+--print('hello world!')
+--print(string.find('wahahahahhaohaoheheheheh', 'hao'))
+
+--local tt = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024 }
+--print(select(1, tt))
+--loginControl = {}
+--loginControl.isOpenRegister = not loginControl.isOpenRegister
+--print(loginControl.isOpenRegister)
+
+------------------------------------------------------------------
+
+-- print(os.date('%h', 1))
+-- print(os.date("%Y-%m-%d %H:%m:%S"))
+-- local time = '12:10:00'
+-- local year = os.date('%Y')
+-- local month = os.date('%m')
+-- local day = os.date('%d')
+-- print(year,month,day)
+-- local dateList = {}
+-- for i = 1, 14 do
+--     local date = os.time({ year = year, month = month, day = day })
+--     dateList[i] = string.format('%s %s', os.date("%Y-%m-%d", date), time)
+--     day = day + 1
+-- end
+-- print(table.concat(dateList, ';\n'))
+------------------------------------------------------------------
+
+--a = {}
+--a['1'] = 1
+--a['2'] = 2
+--a['5'] = 3
+----print(#a)
+--a['4'] = 4
+----print(#a)
+
+--for i, v in pairs(a) do
+--	print(i, v)
+--end
+
+--print(getmetatable("hello world"))
+--print(getmetatable(666))
+
+--local x = math.pi
+--print(x)
+--print(x - x % 0.01)
+--print(math.e)
+
+------------------------------------------------------------------
+
+--local tt = {}
+--tt.a = 1
+--tt.b = 'rmb'
+--tt.c = 66
+
+--local tx = { 5, 'he', 66 }
+
+--print(table.concat(tt, ';'))
+--print(table.concat(tx, ';'))
+
+------------------------------------------------------------------
+
+--bb = { [0] = '0', [1] = '1', [2] = '2' }
+--print(#bb)
+--print(bb[0])
+--for i, v in ipairs(bb) do
+--	print(i, v)
+--end
+
+------------------------------------------------------------------
+
+--print(os.date("%Y-%m-%d %H:%M:%S", os.time()))
+--print(os.time())
+
+------------------------------------------------------------------
+
+--function encodeURI(s)
+--	s = string.gsub(s, "([^%w%.%- ])", function(c)
+--		return string.format("%%02X", string.byte(c))
+--	end)
+--	return string.gsub(s, " ", "+")
+--end
+
+--function decodeURI(s)
+--	    s = string.gsub(s, '%%(%x%x)', function (h) return string.char(tonumber(h, 16))
+--	end )
+--	    return s
+--end
+
+--function encodeURI(s)
+--	    s = string.gsub(s, "([^%w%.%- ])", function (c) return string.format("%%%02X", string.byte(c))
+--	end)
+--	    return  string.gsub(s, " ", "+")
+--end
+
+--local url = 'https://game.lzgame.top/index/game/play?bundle_id=com.yjjxwl.ios&payparam={%22sid%22:%2216536%22,%22uid%22:%22594203%22,%22vip%22:%220%22,%22apple%22:%22com.yjzx.30%22,%22goods%22:%22300元宝%22,%22fee%22:%2230%22,%22attach%22:%22role=179323032|device=2|item=3%22}&uid=594203&token=5574ed8d2c29fe7377426ff959e2c4c2&vip=0&currencycode=CN&countrycode=CN&wxjbtoapp=1&getorder=1&pf=3&inm=1&rfapp=1&gameid=55&exmodel=iPhone9,1&version=1.0&sdkver=1.3&sdk=10&out_trade_no=55-84254-1575422511'
+--url = string.gsub(url, '{', '%%7B')
+--url = string.gsub(url, '}', '%%7D')
+--url = string.gsub(url, '|', '%%7C')
+--url = string.gsub(url, '元宝', '%%E5%%85%%83%%E5%%AE%%9D')
+--print(url)
+--local url2 = 'https%3A%2F%2Fgame%2Elzgame%2Etop%2Findex%2Fgame%2Fplay%3Fbundle%5Fid%3Dcom%2Eyjjxwl%2Eios%26payparam%3D%7B%2522sid%2522%3A%252216536%2522%2C%2522uid%2522%3A%2522594203%2522%2C%2522vip%2522%3A%25220%2522%2C%2522apple%2522%3A%2522com%2Eyjzx%2E30%2522%2C%2522goods%2522%3A%2522300%E5%85%83%E5%AE%9D%2522%2C%2522fee%2522%3A%252230%2522%2C%2522attach%2522%3A%2522role%3D179323032%7Cdevice%3D2%7Citem%3D3%2522%7D%26uid%3D594203%26token%3D5574ed8d2c29fe7377426ff959e2c4c2%26vip%3D0%26currencycode%3DCN%26countrycode%3DCN%26wxjbtoapp%3D1%26getorder%3D1%26pf%3D3%26inm%3D1%26rfapp%3D1%26gameid%3D55%26exmodel%3DiPhone9%2C1%26version%3D1%2E0%26sdkver%3D1%2E3%26sdk%3D10%26out%5Ftrade%5Fno%3D55%2D84254%2D1575422511'
+
+--print(lua_util.url_encode(url))
+
+------------------------------------------------------------------
+
+--local date = os.time({ year = 2018, month = 8, day = 17, hour = 0, minute = 0, second = 0 })
+--print(date)
+--print(tostring(os.time()))
+
+------------------------------------------------------------------
+
+-- function f1(n)
+-- 	--函数参数也是局部变量
+-- 	local function f2()
+-- 		print(n) --引用外包函数的局部变量
+-- 	end
+-- 	return f2
+-- end
+-- g1 = f1(100)
+-- g1() --打印出100
+-- g2 = f1(200)
+-- g2() --打印出200
+-- g1() --打印出100
+
+-- print('--------------')
+
+-- function f1(n)
+-- 	local a = 1
+-- 	local function f2()
+-- 		print('n:' .. n)
+-- 		print('a:' .. a)
+-- 	end
+-- 	n = n + 10
+-- 	a = a + 1
+-- 	return f2
+-- end
+
+-- g1 = f1(300)
+-- g1() --打印出310
+
+------------------------------------------------------------------
+
+-- local jsonstr = '{"kkfix" =>> "1","kk" =>> "(\n{\ntit=\"\\U8d26\\U53f7\\U4e2d\\U5fc3\";\nui=\"aH\"}\n)"}'
+-- -- jsonstr = string.gsub(jsonstr, '\\n', '\n')
+-- -- jsonstr = string.gsub(jsonstr, '\\\"', '\"')
+-- -- jsonstr = string.gsub(jsonstr, '\"{', '{')
+-- -- jsonstr = string.gsub(jsonstr, '}\"', '}')
+-- jsonstr = string.gsub(jsonstr, '%(.+%)', 'nil')
+-- print(jsonstr)
+
+------------------------------------------------------------------
+
+-- local index = string.find('test%sff','%s')
+-- print(index)
+
+------------------------------------------------------------------
+
+-- local str = 'abcdefghijklmnopqrstuvwxyz'
+-- print(string.sub(str,20,30))
+
+------------------------------------------------------------------
+
+-- local url = 'http://cdn.tramtienquyet.vn/viet_res_ios/publish/iOS/CDN.json'
+-- local regex = '/iOS.+CDN.json'
+-- print(string.find(url,'httsp'))
+-- print(string.sub(url,string.find(url,regex)))
+
+------------------------------------------------------------------
+-- math.randomseed(os.time())
+
+-- local items = {
+-- 	{'A',200},
+-- 	{'B',150},
+-- 	{'C',100},
+-- 	{'D',50},
+-- 	{'E',40},
+-- 	{'F',30},
+-- 	{'G',20},
+-- 	{'H',10},
+-- }
+-- local all = 0
+-- for i,v in ipairs(items) do
+-- 	all = all + v[2]
+-- end
+
+-- local function getlottery(items,index)
+-- 	local sum = 0
+-- 	for i,v in ipairs(items) do
+-- 		sum = sum + v[2]
+-- 		if index<=sum then
+-- 			return v[1]
+-- 		end
+-- 	end
+-- end
+
+-- for i=1,1 do
+-- 	local index = math.random(all)
+-- 	print(getlottery(items,index),index)
+-- end
+
+------------------------------------------------------------------
+
+local function tableToUrl(data)
+	local url = ''
+	local first = true
+	for k, v in pairs(data) do
+		if first then
+			first = nil
+			url = string.format('%s=%s',k,v)
+		else
+			url = string.format('%s&%s=%s',url,k,v)
+		end
+	end
+	return url
+end
+
+local function tableToArray(data)
+	local array = {}
+	for k, v in pairs(data) do
+		table.insert(array, k)
+		table.insert(array, v)
+	end
+	return array
+end
+
+local info = {}
+info.istest = 1
+info.account = 'userid'
+info.serverId = 'serverid'
+info.role = 'roleid'
+info.item = 'itemid'
+info.payType = 1
+info.receipt_data = 'test'
+
+--print(tableToUrl(info))
+local array = tableToArray(info)
+print(table.concat(array,','))
+
+------------------------------------------------------------------
+
+------------------------------------------------------------------
+------------------------------------------------------------------
+------------------------------------------------------------------
+------------------------------------------------------------------
+------------------------------------------------------------------
+------------------------------------------------------------------
---
+++ test.py
@@ -0,0 +1,249 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+#---------------------------------------------------------#
+
+# if ('/Bugly/' in '/iOS/Bugly/Bugly.framework/Bugly'):
+# 	print('true')
+# else:
+# 	print('false')
+
+#---------------------------------------------------------#
+
+# line = 'http://n1041yjzxgb.xw66.top/foaalpios/login/serverlist '
+# index = line.index(':')
+# key = line[:index].strip()
+# value = line[index+1:].strip()
+# if key=='http':
+# 	print(value)
+# 	array = value.split('/')
+# 	print(array)
+# 	print(array[2])
+# else:
+# 	print(value)
+
+#---------------------------------------------------------#
+
+# import xlrd
+# from datetime import date,datetime
+
+# wb = xlrd.open_workbook('/Users/leo/Desktop/IOS内购产品ID.xlsx')#打开文件
+# print(wb.sheet_names())#获取所有表格名字
+# sheet1 = wb.sheet_by_index(0)#通过索引获取表格
+# #sheet2 = wb.sheet_by_name('Sheet1')#通过名字获取表格
+# #print(sheet1,sheet2)
+# #print(sheet1.name,sheet1.nrows,sheet1.ncols)
+# #rows = sheet1.row_values(2)#获取行内容
+# #cols = sheet1.col_values(3)#获取列内容
+# #print(rows)
+# #print(cols)
+# rmblist = sheet1.col_values(1)#获取rmb内容
+# itemIds = sheet1.col_values(3)#获取产品ID内容
+# itemIDs = {}
+# for i in range(2, len(itemIds)):
+# 	key = rmblist[i].encode('utf-8').replace('元','').strip()
+# 	itemIDs[key]=itemIds[i]
+# for key,value in itemIDs:
+# 	print(key,value)
+# #print(sheet1.cell(1,0).value)#获取表格里的内容，三种方式
+# print(sheet1.cell_value(1,0))
+# #print(sheet1.row(1)[0].value)
+
+#---------------------------------------------------------#
+
+# import pandas
+# import numpy
+
+# path = '/Users/leo/Desktop/IOS内购产品ID.numbers'
+# data_file = pd.read_csv(path)
+# print (data_file[0:3])
+
+#---------------------------------------------------------#
+
+# L1 = ['AdmIn','anny','LUCY','sandY','wILl']
+# def normallize(name):
+# 	#return name.upper()#全大写
+# 	#return name.capitalize()#首字母大写其余小写
+# 	return name.lower()#全小写
+# L2 = list(map(normallize,L1))
+# print(L2)
+
+#---------------------------------------------------------#
+
+# import os
+# import sys
+
+# print('sys.path:'+sys.path[0])
+# print('sys.argv:'+sys.argv[0])
+# print('__file__:'+__file__)
+# print('os.getcwd():'+os.getcwd())
+# print('os.path.realpath():'+os.path.realpath(__file__))
+# print('(os.path.abspath(__file__)):'+os.path.abspath(__file__))
+# print('os.path.dirname(os.path.realpath()):'+os.path.dirname(os.path.realpath(__file__)))
+# print('os.path.dirname(os.path.abspath(__file__)):'+os.path.dirname(os.path.abspath(__file__)))
+
+#---------------------------------------------------------#
+
+# import os
+# import sys
+# print(sys.path)
+
+#---------------------------------------------------------#
+
+# list1 = [9,8,7,6,5]
+# list1.sort()
+# print list1
+
+#---------------------------------------------------------#
+
+import os
+
+path = '/Users/m5pro/Documents/M5-C/release-2.0/Assets/Scripts'
+for root, directory, files in os.walk(path):  # 当前根,根下目录,目录下的文件
+	for filename in files:
+		name, suf = os.path.splitext(filename)  # 文件名,文件后缀
+		if suf == '.cs':
+			print os.path.join(root, filename)  # 把一串字符串组合成路径
+
+#---------------------------------------------------------#
+
+# import re
+# # str = "if (info && 0 < [info[@\"sfsess\"] length])"\
+# #       "if (info && 0 == [info[@\"sfsess\"] length])"\
+# #       "if (info && 0 <= [info[@\"sfuid\"] length])"
+# # key = re.findall("if.*?\(.+?\)",str)
+# str="NSString *nstr=@\"Counrt\";"\
+#     "if (c == d)"\now = int(time.time())
+#     "NSData *data = [wd daf];"\
+#     "bool b = c == d;"
+# key = re.findall("[^=]+?[=]{1}[^=]+?;",str)
+# print(key)
+
+#---------------------------------------------------------#
+
+# import time
+# print time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+# print str(int(time.time()))
+
+#---------------------------------------------------------#
+
+# str1 = '/Users/m5pro/Documents/M5-C/ios-release-2.0/VerifyAssets/Mods'
+# li = str1.rindex('/');
+# print str1[0:li]
+
+#---------------------------------------------------------#
+
+# str1 = '666'
+# iii = 666
+# print type(str1)==str
+# print type(iii)
+# print type(iii)==int
+
+#---------------------------------------------------------#
+
+# import os
+# import zipfile
+
+# skin_path = "/Users/m5pro/Documents/M5-C/release-2.0/VerifyAssets/Mods/Mods_OnlyTest/iOS/skin"
+# skin_zip = "{0}.zip".format(skin_path)
+# with zipfile.ZipFile(skin_zip, 'w') as target:
+# 	for i in os.walk(skin_path):
+# 		for n in i[2]:
+# 			path = ''.join((i[0], '/', n))
+# 			zip_path = path.replace(skin_path, 'skin')
+# 			target.write(path, zip_path)
+
+#---------------------------------------------------------#
+
+# class MyTest():
+# 	def sayhi(self):
+# 		print('hello world.')
+
+# 	def func1(self):
+# 		self.items={}
+
+# 	def func2(self):
+# 		if hasattr(self, 'items'):
+# 			print('yes')
+# pass
+
+# obj = MyTest()
+# obj.sayhi()
+# obj.func1()
+# obj.func2()
+
+#---------------------------------------------------------#
+
+# import Tkinter
+# top = Tkinter.Tk()
+# top.mainloop()
+
+#---------------------------------------------------------#
+
+# import wx  # 导入wx包
+# app = wx.App()  # 创建一个应用程序对象
+# frm = wx.Frame(None, title="Hello World")  # 创建一个界面
+# frm.Show()  # 显示它
+# app.MainLoop()   # 启动一个时间循环
+
+#---------------------------------------------------------#
+
+# str1='Hello'
+# print 'h' in str1 
+# print 'H' in str1
+# num1=[1,2]
+# print 1 in num1
+# table = {'int':'1','float':'2','long':'3'}
+# print None in table
+# print '%s,%d,%2.2f' % ('test',2,134.1534)
+
+#---------------------------------------------------------#
+
+# import re
+# func_regex = re.compile(r'\w+\s+\w+\s*\(\w*\)\s*\{')   # 查找数字
+# name_regex = re.compile(r'\b\w+\s*\(')
+
+# with open('/Users/m5pro/Documents/M5-C/release-2.0/Assets/Editor/MixResource/FileNameTranslatorEditor.cs') as file:
+# 	content = file.read()
+# 	result = func_regex.findall(content)
+# #print result
+# with open('/Users/m5pro/Documents/func_name.txt', 'w+') as f:
+# 	for v in result:
+# 		array = name_regex.findall(v)
+# 		if len(array)==1:
+# 			f.write(array[0][:-1].strip()+'\n')
+
+#---------------------------------------------------------#
+
+
+
+#---------------------------------------------------------#
+
+
+
+#---------------------------------------------------------#
+
+
+
+#---------------------------------------------------------#
+
+
+
+#---------------------------------------------------------#
+
+
+
+#---------------------------------------------------------#
+
+
+
+#---------------------------------------------------------#
+
+
+
+#---------------------------------------------------------#
+
+
+
+#---------------------------------------------------------#
+
+

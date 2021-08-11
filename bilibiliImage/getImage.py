e0369a58a929292ef1cd6c0addc685bfe167dfe9
unknown <yh821@qq.com>
2021-08-11 19:55:41 +0800

    添加模块检查

--- bilibiliImage/getImage.py
+++ bilibiliImage/getImage.py
@@ -3,11 +3,45 @@
 
 import sys
 import re
-import requests
-import bs4
 import urllib
 from collections import deque
+
+try:
+    import requests
+except:
+    print ("正在安装requests模块，请等待...")
+    success = os.system('pip install requests')
+    if success == 0:
+        print("requests安装成功")
+        import requests
+    else:
+      print ("requests安装失败")
+      quit()
+
+try:
+    import wx
+except:
+    print ("正在安装wxpython模块，请等待...")
+    success = os.system('pip install wxpython')
+    if success == 0:
+        print("wxpython安装成功")
+        import wx
+    else:
+      print ("wxpython安装失败")
+      quit()
 from BaseForm import *
+
+try:
+    import bs4
+except:
+    print ("正在安装bs4模块，请等待...")
+    success = os.system('pip install beautifulsoup4')
+    if success == 0:
+        print("bs4安装成功")
+        import bs4
+    else:
+      print ("bs4安装失败")
+      quit()
 
 header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36 Edg/86.0.622.51"}
 
